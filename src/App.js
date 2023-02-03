370b3e12eddbd7937913f9b82b9b5ff8ea624f9b
Ian Ruvuto <tallerian5@gmail.com>
2023-02-03 16:58:18 +0100

    Input and placeholder

--- src/App.js
+++ src/App.js
@@ -10,18 +10,27 @@ function App() {
       <div class = "chatbox">
 
         <div class = "user_chat_container">
-
-        <div class = "container_chat">
-          <div class = "title_txt">Hello Welcome to my chat box</div>
-        </div>
-
-        <div class = "container_user">
-          <div class = "user">
-            <div id = "user_img"></div>
-            <div class = "user_name">{username}</div>
-          </div>
+        
+          <div class = "container_chat">
           
-        </div>
+            <div class = "title_txt">Hello Welcome to my chat box</div>
+            <div class = "container_sms">
+              <div class="chat_display"></div>
+
+              <div class = "chat_text">
+                <input type="text" placeholder='Type something...' />
+              </div>
+              
+            </div>
+          </div>
+
+
+          <div class = "container_user">
+            <div class = "user">
+              <div id = "user_img"></div>
+              <div class = "user_name">{username}</div>
+            </div>
+          </div>
 
         </div>
 
